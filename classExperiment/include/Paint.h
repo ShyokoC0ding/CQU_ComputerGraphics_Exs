49fdca8ff443f7f3b71d0e27d7355d9a9190bdc5
Shyoko <leirishyoko@gmail.com>
2024-10-11 12:56:35 +0800

    Add files via upload

---
+++ classExperiment/include/Paint.h
@@ -0,0 +1,11 @@
+//
+// Created by 84151 on 2024/10/2.
+//
+
+#ifndef OPENGL_PAINT_H
+#define OPENGL_PAINT_H
+
+void init();
+void drawAxis(float minX, float maxX, float minY, float maxY, int numLines);
+
+#endif //OPENGL_PAINT_H
